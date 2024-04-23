e23ddbd2e81c55f117bec80c72196ee1578a375f
abs7798 <abs44@126.com>
2024-04-23 12:13:18 +0800

    dsd

--- app.py
+++ app.py
@@ -1,30 +1,33 @@
 import gradio as gr
-from lmdeploy import pipeline, TurbomindEngineConfig
 import os
 
 # download internlm2 to the base_path directory using git tool
-base_path = './internlm2-chat-7b'
-os.system(f'mkdir internlm2-chat-7b')
+base_path = './my_internlm_model'
+os.system(f'mkdir my_internlm_model')
 
 print(f'创建文件夹{os.path.isdir(base_path)}')
 os.system(f'cd {base_path}')
-
 os.system("git lfs install")
 os.system(f'git clone https://code.openxlab.org.cn/abs7798/my_internlm_model.git')
 os.system(f'cd {base_path} && git lfs pull')
 os.system("pip install sentencepiece")
 os.system("pip install einops")
+os.system("pip install lmdeploy[all]==0.3.0")
 
-backend_config = TurbomindEngineConfig(session_len=8192,cache_max_entry_count=0.2) # 图片分辨率较高时请调高session_len
-# pipe = pipeline('liuhaotian/llava-v1.6-vicuna-7b', backend_config=backend_config) 非开发机运行此命令
-pipe = pipeline(base_path, backend_config=backend_config)
+import gradio as gr
+from lmdeploy import pipeline, TurbomindEngineConfig
+backend_config = TurbomindEngineConfig(session_len=8192,cache_max_entry_count=0.7) # 图片分辨率较高时请调高session_len
+pipe = pipeline(base_path, model_name='my_model', backend_config=backend_config)
 
-def model(image, text):
-    if image is None:
-        return [(text, "请上传一张图片。")]
-    else:
-        response = pipe((text, image)).text
-        return [(text, response)]
+#'''text= '你是谁'
+#response = pipe([text])
+#print(response[0].text)'''
 
-demo = gr.Interface(fn=model, inputs=[gr.Image(type="pil"), gr.Textbox()], outputs=gr.Chatbot())
-demo.launch()   
+def model(text):
+    response = pipe([text])
+    print(response)
+    res = response[0].text
+    return [(text, res)]
+
+demo = gr.Interface(fn=model, inputs= gr.Textbox(), outputs=gr.Chatbot())
+demo.launch()
